ac414e966ed4b17557a119e6c2b7948b0de025f6
jinfengwang <jinfeng.wang@razer.com>
2025-03-14 15:40:08 +0800

    update mssql connection string

--- src/jfYu.Core.Test/jfYu.Core.Test/Startup.cs
+++ src/jfYu.Core.Test/jfYu.Core.Test/Startup.cs
@@ -34,7 +34,7 @@ namespace jfYu.Core.Test
 
             var databaseType = DatabaseType.SqlServer;
 
-            var connectionString = "Data Source = 127.0.0.1; database = Test; User Id = sa; Password = StrongP@ssw0rd!;";
+            var connectionString = "Data Source = 127.0.0.1; database = Test; User Id = sa; Password = StrongP@ssw0rd!;MultipleActiveResultSets=true;";
 
             services.AddJfYuDbContextService<DataContext>(q =>
             {
--- src/jfYu.Core/jfYu.Core.Data/README.md
+++ src/jfYu.Core/jfYu.Core.Data/README.md
@@ -13,7 +13,7 @@ Configuration
 ```
  "ConnectionStrings": {
     "DatabaseType": "SqlServer",
-    "ConnectionString": "Data Source = 127.0.0.1,9004; database = dbtest; User Id = sa; Password = 123456;",
+    "ConnectionString": "Data Source = 127.0.0.1,9004; database = dbtest; User Id = sa; Password = 123456;MultipleActiveResultSets=true;",
     "JfYuReadOnly":"JfYuReadOnly",
     "ReadOnlyDatabases": [
       {
