a77d33018d6e395d8f4add2b3cfc0b7a01b1fc0e
jinfeng.wang <jinfeng.wang@RZB-CN-CD-115>
2025-04-14 11:29:32 +0800

    update  RabbitMQ to support multitasking

--- src/jfYu.Core/jfYu.Core.RabbitMQ/ContainerBuilderExtensions.cs
+++ src/jfYu.Core/jfYu.Core.RabbitMQ/ContainerBuilderExtensions.cs
@@ -15,26 +15,21 @@ namespace jfYu.Core.RabbitMQ
         /// Adds RabbitMQ services to the IServiceCollection.
         /// </summary>
         /// <param name="services">The IServiceCollection to add the services to.</param>
-        /// <param name="configureConnectionFactory">An action to configure the ConnectionFactory and MessageRetryPolicy.</param>
+        /// <param name="configure">An action to configure the ConnectionFactory and MessageRetryPolicy.</param>
         /// <returns>The IServiceCollection with the RabbitMQ services added.</returns>
         /// <exception cref="ArgumentNullException">Thrown when configureConnectionFactory is null.</exception>
-        public static IServiceCollection AddRabbitMQService(this IServiceCollection services, Action<ConnectionFactory, MessageRetryPolicy> configureConnectionFactory)
+        public static IServiceCollection AddRabbitMQService(this IServiceCollection services, Action<ConnectionFactory, MessageRetryPolicy> configure)
         {
-            if (configureConnectionFactory == null)
-                throw new ArgumentNullException(nameof(configureConnectionFactory), "The configureConnectionFactory action cannot be null.");
+            if (configure == null)
+                throw new ArgumentNullException(nameof(configure), "The configureConnectionFactory action cannot be null.");
 
-            var option = new ConnectionFactory();
-            var messageRetryPolicy = new MessageRetryPolicy();
-            configureConnectionFactory(option, messageRetryPolicy);
-
-            var connection = option.CreateConnection();
-            services.AddSingleton(connection);
-            services.AddSingleton(messageRetryPolicy);
-            services.AddTransient(provider =>
-            {
-                return connection.CreateModel();
-            });
-            services.AddSingleton<IRabbitMQService, RabbitMQService>();
+            var factory = new ConnectionFactory();
+            var retryPolicy = new MessageRetryPolicy();
+            configure(factory, retryPolicy);
+            services.AddScoped(_ => factory.CreateConnection());
+            services.AddSingleton(retryPolicy);
+            services.AddScoped(provider => provider.GetRequiredService<IConnection>().CreateModel());
+            services.AddScoped<IRabbitMQService, RabbitMQService>();
             return services;
         }
     }
